3799852763d30bba5a0f88775f7e7c40107d2358
Pedro Castro <pgkastro@gmail.com>
2022-11-28 12:06:51 -0300

    projeto finalizado

--- Script/script.js
+++ Script/script.js
@@ -2,3 +2,24 @@ const adviceId = document.querySelector(".advice-id");
 const adviceText = document.querySelector("blockquote");
 const adviceButton = document.querySelector("button");
 
+adviceButton.addEventListener("click", getAdvice);
+
+window.onload = () => {
+  getAdvice();
+};
+
+function getAdvice() {
+  fetch("  https://api.adviceslip.com/advice")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const advice = data.slip;
+
+      adviceText.innerHTML = `"${advice.advice}"`;
+      adviceId.innerHTML = `#${advice.id}`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
